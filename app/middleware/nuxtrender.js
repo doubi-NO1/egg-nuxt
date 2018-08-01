758f327ba7d72d6919c6a366a33c839500891238
iSword Wilson <546645810@qq.com>
2018-08-01 16:50:46 +0800

    修复nuxt+egg在开发环境下无法热更新
    因egg自动给content-length赋值，长度为20，影响了socket-io的持续加载，去掉即可热更新。

--- app/middleware/nuxtrender.js
+++ app/middleware/nuxtrender.js
@@ -2,27 +2,31 @@
 
 module.exports = function (options, app) {
   return async (ctx, next) => {
-    // webpack hot reload
-    //if (ctx.path !== '/__webpack_hmr') {
-      await next();
-      // ignore status if not 404
-      if (ctx.status !== 404 || ctx.method !== 'GET') {
-        return;
-      }
+    
+    await next();
+    // ignore status if not 404
+    if (ctx.status !== 404 || ctx.method !== 'GET') {
+      return;
+    }
 
-      ctx.status = 200;
-      const path = ctx.path;
-      if (/\.js$/.test(path)) {
-        ctx.set('Content-Type', 'application/javascript');
-      }
-      if (/\.css/.test(path)) {
-        ctx.set('Content-Type', 'text/css');
-      }
-      // the "nuxt.render" returns callback, not Promise
-      await new Promise(resolve => {
-        app.nuxt.render(ctx.req, ctx.res, resolve);
-      });
-      next();
-    //}
+    ctx.status = 200;
+    const path = ctx.path;
+    if (/\.js$/.test(path)) {
+      ctx.set('Content-Type', 'application/javascript');
+    }
+    if (/\.css/.test(path)) {
+      ctx.set('Content-Type', 'text/css');
+    }
+    // webpack hot reload
+    // egg will set 'content-length' with value, it will disable the hot middleware keep alive.
+    // egg 默认设置了 'content-length' 值，导致热更新失败，建议去掉该值保持活跃
+    if (ctx.path === '/__webpack_hmr') { 
+      ctx.response.remove('content-length')
+    }
+    // the "nuxt.render" returns callback, not Promise
+    await new Promise(resolve => {
+      app.nuxt.render(ctx.req, ctx.res, resolve);
+    });
+    next();
   }
 };
